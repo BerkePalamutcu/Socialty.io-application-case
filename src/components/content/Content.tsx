c9de88fe137714e55127f0521fbc3bf616326ea3
navarcus <bspalamutcu@gmail.com>
2022-10-10 15:05:31 +0300

    console.log removed

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -15,8 +15,6 @@ const Content: React.FC<any> = () => {
   const pending: any = useAppSelector((state) => state.apiData.loading);
   const errorMessage: any = useAppSelector((state) => state.apiData.error);
 
-  console.log(apiData);
-
   React.useEffect(() => {
     dispatch(fetchApiData());
   }, []);
